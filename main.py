4a6a9ea19a1e2903fc5230eadd0f497416850d91
Zuev Vladislav <v.zuev@jump.finance>
2022-12-25 18:48:39 +0300

    Fix

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_list_phone():
     buf = None
 
     try:
-        with open(f'{os.getcwd()}/db.txt') as file:
+        with open(f'{os.getcwd()}/db.txt', mode='r', encoding='utf-8') as file:
             buf = file.readlines()
 
         index = 0
@@ -26,7 +26,7 @@ def get_text_msg():
     buf = None
 
     try:
-        with open(f'{os.getcwd()}/text.txt') as file:
+        with open(f'{os.getcwd()}/text.txt', mode='r', encoding='utf-8') as file:
             buf = file.read()
     except FileNotFoundError:
         print('Не найден файл с номерами! Проверьте, что есть файл text.txt в этой папке!')
@@ -34,27 +34,24 @@ def get_text_msg():
     return buf
 
 
-def send_msg(phones_list, text, every_minute=True):
-    for el in phones_list:
+def send_msg(phones_list, text_sms, every=True):
+    for element in phones_list:
         today = datetime.datetime.now()
 
-        hour = today.hour
+        hour = 0 if today.minute == 57 and today.hour == 23 else today.hour
         minute = today.minute
 
-        if every_minute:
-            minute = minute + 1 if minute < 59 else 0
+        if every:
+            minute = minute + 2 if minute < 57 else 0
         else:
-            minute = random.randint(minute, 59) if minute < 58 else random.randint(1, 3)
+            minute = random.randint(minute, 57) if minute < 57 else random.randint(1, 3)
 
-        hour = hour + 1 if hour < 23 else 0
 
-        pywhatkit.sendwhatmsg(f"+{el}", text, hour, minute, tab_close=True, close_time=3)
+        pywhatkit.sendwhatmsg(f"+{element}", text_sms, hour, minute, tab_close=True, close_time=3)
 
 
 if __name__ == '__main__':
-    every_minute = input('Отправка каждую минуту? (y - да/n - в случайное кол-во минут) ').lower()
-
-    print(every_minute not in ['y', 'n'])
+    every_minute = input('Отправка каждые 3 минуты? (y - да/n - в случайное кол-во минут) ').lower()
 
     if every_minute not in ['y', 'n']:
         print('Введено неверное значение')
